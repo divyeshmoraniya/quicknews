ce5ed86bc6b61fd8ab03dff858f60739ffdb8be1
divyeshmoraniya <hdivyesh62@gmail.com>
2024-10-14 19:55:56 +0530

    Update News.jsx

--- quicknews/src/assets/component/News.jsx
+++ quicknews/src/assets/component/News.jsx
@@ -1,124 +1,68 @@
-import React, { Component } from 'react';
-import Newsitem from './newsitem';
+import React, { useState, useEffect } from 'react';
+import Newsitem from './Newsitem';
 import Spine from './Spine';
-import PropTypes from 'prop-types';
 import InfiniteScroll from 'react-infinite-scroll-component';
-
 import Logostart from './Logostart';
 
-export default class News extends Component {
-    static defaultProps = {
-        category: 'sports'
-    };
-    static propTypes = {
-        category: PropTypes.string
-    };
+export default function News(props) {
+    const [articles, setArticles] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [page, setPage] = useState(1);
+    const [totalResults, setTotalResults] = useState(0);
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            articles: [],
-            loading: false,
-            page: 1,
-            totalResults: 0,
-            setProgress : 0
+    useEffect(() => {
+        const fetchData = async () => {
+            props.setProgress(10);
+            let url = `https://newsapi.org/v2/top-headlines?country=us&category=${props.category}&apiKey=9d4b8772c127454085b200daf7d4c3bd&page=1`;
+            setLoading(true);
+            try {
+                let data = await fetch(url);
+                let passdata = await data.json();
+                props.setProgress(65);
+                setArticles(passdata.articles);
+                setTotalResults(passdata.totalResults);
+                setLoading(false);
+                props.setProgress(100);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
         };
-    }
+        fetchData();
+    }, [props.category]);
 
-    fetchData = async () => {
-        this.props.setProgress(10);
-        this.setState({ page: this.state.page + 1 });
-        let url = `https://newsapi.org/v2/top-headlines?country=us&category=${this.props.category}&apiKey=9d4b8772c127454085b200daf7d4c3bd&page=${this.state.page}`;
-        this.setState({ loading: true });
+    const fetchMoreData = async () => {
+        setPage(page + 1);
+        let url = `https://newsapi.org/v2/top-headlines?country=us&category=${props.category}&apiKey=9d4b8772c127454085b200daf7d4c3bd&page=${page + 1}`;
+
         let data = await fetch(url);
-        this.props.setProgress(30);
         let passdata = await data.json();
-        console.log(passdata);
-        this.props.setProgress(65);
-        this.setState({
-            articles: this.state.articles.concat(passdata.articles),
-            totalResults: passdata.totalResults,
-            loading: false
-        });
-        this.props.setProgress(100);
+        setArticles(articles.concat(passdata.articles));
     };
 
-    async componentDidMount() {
-        this.props.setProgress(10);
-        let url = `https://newsapi.org/v2/top-headlines?country=us&category=${this.props.category}&apiKey=9d4b8772c127454085b200daf7d4c3bd&page=1`;
-        this.setState({ loading: true });
-        let data = await fetch(url);
-        this.props.setProgress(30);
-        let passdata = await data.json();
-        console.log(passdata);
-        this.props.setProgress(65);
-        this.setState({
-            articles: passdata.articles,
-            totalResults: passdata.totalResults,
-            loading: false
-        });
-        this.props.setProgress(100);
-    }
-
-    // nc = async () => {
-    //     if ((this.state.page + 1 >= Math.ceil(this.state.totalResults / 4))) {
-    //         alert("No more news available!");
-    //     } else {
-    //         this.fetchData();
-    //     }
-    // };
-
-    // pc = async () => {
-    //     let url = `https://newsapi.org/v2/top-headlines?country=us&category=${this.props.category}&apiKey=9d4b8772c127454085b200daf7d4c3bd&page=${this.state.page - 1}`;
-    //     this.setState({ loading: true });
-    //     let data = await fetch(url);
-    //     let passdata = await data.json();
-    //     this.setState({
-    //         page: this.state.page - 1,
-    //         articles: passdata.articles,
-    //         loading: false
-    //     });
-    // };
-
-    render() {
-        return (
-            <div className="container my-3 mb-2">
-                <div
-                    className={`bg-${this.props.mode === 'light' ? 'ldark' : 'light'} text-${this.props.mode === 'light' ? 'light' : 'dark'
-                        }`}
-                >
-                    <div className="daily-news">
-                        <h1 className="text-center">Know about {this.props.category} </h1>
-                        <Logostart />
-                    </div>
-
-                    <InfiniteScroll
-                        dataLength={this.state.articles.length}
-                        next={this.fetchData}
-                        hasMore={this.state.articles.length < this.state.totalResults}
-                        loader={<Spine />}
-                    >
-                        <div className="row">
-                            {this.state.articles.map((e) => {
-                                return (
-                                    <div className="col-md-4" key={e.content}>
-                                        <Newsitem
-                                            urlToImage={e.urlToImage}
-                                            description={e.description ? e.description.slice(0, 50) : ""}
-                                            title={e.title ? e.title.slice(0, 30) : ""}
-                                            newsurl={e.url}
-                                            author={e.author}
-                                            source={e.source.name}
-                                        />
-                                    </div>
-                                );
-                            })}
+    return (
+        <div className={`container my-3 bg-${props.mode} text-${props.mode === 'light' ? 'dark' : 'light'}`}>
+            <h1 className="text-center">Know about {props.category}</h1>
+            <Logostart />
+            <InfiniteScroll
+                dataLength={articles.length}
+                next={fetchMoreData}
+                hasMore={articles.length < totalResults}
+                loader={<Spine />}
+            >
+                <div className="row">
+                    {articles && articles.map((e, index) => (
+                        <div className="col-md-4" key={index}>
+                            <Newsitem
+                                title={e.title}
+                                description={e.description}
+                                urlToImage={e.urlToImage}
+                                newsurl={e.url}
+                                source={e.source.name}
+                            />
                         </div>
-                    </InfiniteScroll>
-
-                    
+                    ))}
                 </div>
-            </div>
-        );
-    }
+            </InfiniteScroll>
+        </div>
+    );
 }
